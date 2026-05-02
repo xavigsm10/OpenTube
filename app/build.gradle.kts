2247762dab30bdd48c9f2ff5ef793bd2ebfec8c0
Xavi GSM <xavigsm10@gmail.com>
2026-05-01 20:22:00 -0400

    chore: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.opentube"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.1.9"
+        versionCode = 10
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
